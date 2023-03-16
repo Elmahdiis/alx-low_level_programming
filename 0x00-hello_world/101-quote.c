fff8ebb6c60304813793ae4cef106cc3090a815b
Elmahdiis <mehdiaddi2003@gmail.com>
2023-03-16 13:02:16 -0700

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+    write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
+}
+[20:10]
+#include <stdio.h>
+/**
+ *main - this is the main function
+ *Return: always 0
+ */
+int main(void)
+{    puts(""Programming is like building a multilingual puzzle");
+    return (0);
+}
